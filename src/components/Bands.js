67283b128726c76c7c8ae099bff12f49fa2e6eba
yassimortensen <yassimortensen@gmail.com>
2018-01-11 18:07:16 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render(){
+    const list = this.props.store.getState().bands.map((band, index) => (
+      <li key={index}>{band}</li>
+    ))
     return (
-      <div>
-        band input
-      </div>
+      <ul>
+        {list}
+      </ul>
     );
   }
 };
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,8 +1,7 @@
-export default function manageBand(state = {
-  bands: [],
-}, action) {
+export default function manageBand(state = {bands: []}, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      // console.log(action)
       return Object.assign({}, state, { bands: state.bands.concat(action.band) });
     default:
       return state;
